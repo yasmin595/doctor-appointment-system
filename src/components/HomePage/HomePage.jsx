864b0ca9353092f3473a160f0b93e2e4bf5e66b2
fahadbgnr <aminulislamfahad1@gmail.com>
2025-09-06 01:39:49 +0600

    have any qus and contactUs section done

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,6 +3,8 @@ import Banner from './Banner/Banner'
 import HowItWorks from './HowItWorks/HowItWorks'
 import PatientFeedback from './PatientFeedback/PatientFeedback'
 import WhyDocNow from './WhyDocNow/WhyDocNow'
+import FAQSection from './FAQSection/FAQSection'
+import ContactUs from './ContactUs/ContactUs'
 
 export default function HomePage() {
   return (
@@ -25,6 +27,16 @@ export default function HomePage() {
        {/* WhyDocNow Section */}
       <section className="mt-16 bg-green-50 dark:bg-gray-900">
         <WhyDocNow/>
+      </section>
+
+      {/* FAQ Section */}
+      <section className="mt-16 bg-green-50 dark:bg-gray-900">
+        <FAQSection/>
+      </section>
+
+      {/* ContactUs Section */}
+      <section className="mt-16 bg-green-50 dark:bg-gray-900">
+        <ContactUs/>
       </section>
     </div>
   )
