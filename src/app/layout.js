4118689b4ab52880aa2f5b49cf2efc9c00fe1150
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-09-05 21:40:51 +0600

    added and solve the some problem

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,7 +43,7 @@ export default function RootLayout({ children }) {
           {children}
            <Toaster richColors />
             <Footer></Footer>
- development
+ 
         </ThemeProvider>
       </NextAuthProvider>
 
