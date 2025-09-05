cb3d9d4892abb04729e53e0142e72678274d1a49
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-09-05 15:18:21 +0600

    added

--- src/app/all-doctor/page.jsx
+++ src/app/all-doctor/page.jsx
@@ -18,6 +18,7 @@ export default function AllDoctorPage() {
             .then((data) => setDoctors(data));
     }, []);
 
+    
     return (
         <div className="max-w-6xl mx-auto p-6">
             <h1 className="text-3xl font-bold text-center mb-8">All Doctors</h1>
